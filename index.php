51b83d4f4612ea5bedb27f37df147aa94e947ea0
protodeleter <pavel.nsnv@gmail.com>
2020-06-21 21:53:53 +0300

    ...

--- index.php
+++ index.php
@@ -5,15 +5,16 @@ use Models\User;
 use Models\Article;
 use Models\News;
 
+
 spl_autoload_register(function ($class) {
     require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
 });
 
 $data = Product::findAll();
 //var_dump($data);
-
 $data = User::findAll();
 //var_dump($data);
+
 
 $db = new Db();
 
